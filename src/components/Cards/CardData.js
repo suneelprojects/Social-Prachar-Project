cb3b720a6aae7458fc2dd111a42b3b9b0833cec8
vinodparepalli <parepallivinod@gmail.com>
2024-08-20 15:39:47 +0530

    vinod mega drive day

--- src/components/Cards/CardData.js
+++ src/components/Cards/CardData.js
@@ -1,11 +1,11 @@
-import img1 from '../Assets/img1.jpg';
-import img2 from '../Assets/img2.jpg';
-import img3 from '../Assets/img3.jpg';
-import img4 from '../Assets/img4.jpg';
-import img5 from '../Assets/img5.jpg';
-import img6 from '../Assets/img6.jpg';
-import img7 from '../Assets/img7.jpg';
-import img8 from '../Assets/img8.jpg';
+import img1 from '../../assets/img1.jpg';
+import img2 from '../../assets/img2.jpg';
+import img3 from '../../assets/img3.jpg';
+import img4 from '../../assets/img4.jpg';
+import img5 from '../../assets/img5.jpg';
+import img6 from '../../assets/img6.jpg';
+import img7 from '../../assets/img7.jpg';
+import img8 from '../../assets/img8.jpg';
 
 export const data = [
     {
