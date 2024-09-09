612a06c330dc71503e9b354b79dacfbb91e5f386
gandhamgangabhavani <gandhamgangabhavani0@gmail.com>
2024-09-09 13:08:26 +0530

    ganga changes

--- src/components/SignInForm/SignInform.js
+++ src/components/SignInForm/SignInform.js
@@ -1,9 +1,44 @@
-import React from 'react';
-import SignInFormCSS from './SignInForm.module.css'; // Adjust path as necessary
+import React, { useState } from 'react';
+import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
+import SignInFormCSS from './SignInForm.module.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'; // Adjust path as necessary
+import { Link, useNavigate } from 'react-router-dom';
 
 const SignInForm = ({ onClose }) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+    const auth = getAuth();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            toast.success('Login Successfully');
+            navigate('/profile');
+        } catch (err) {
+            toast.error(err.message);
+        }
+        setEmail("");
+        setPassword("");
+    };
+
+    const triggerResetEmail = async () => {
+        try {
+            await sendPasswordResetEmail(auth, email);
+            toast.success('Password reset email sent');
+        } catch (error) {
+            toast.error(error.message);
+        }
+    };
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
+    const togglePasswordVisibility = () => {
+      setPasswordVisible(!passwordVisible);
+    };
     return (
-        <div className={SignInFormCSS.SignInForm}>
+        <form className={SignInFormCSS.SignInForm} onSubmit={handleSubmit}>
             <button className={SignInFormCSS.closeButton} onClick={onClose}>
                 &#x2715;
             </button>
@@ -15,11 +50,15 @@ const SignInForm = ({ onClose }) => {
                     type="text"
                     placeholder="Username or Email Address"
                     className={SignInFormCSS.input}
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                 />
                 <input
                     type="password"
                     placeholder="Password"
                     className={SignInFormCSS.input}
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                 />
             </div>
             <div className={SignInFormCSS.options}>
@@ -30,13 +69,14 @@ const SignInForm = ({ onClose }) => {
                     />
                     <span>Keep me signed in</span>
                 </label>
-                <button className={SignInFormCSS.forgotButton}>Forgot?</button>
+                <button className={SignInFormCSS.forgotButton} onClick={triggerResetEmail}>Forgot?</button>
             </div>
             <div className={SignInFormCSS.actions}>
                 <button className={SignInFormCSS.signInButton}>Sign In</button>
-                <p className={SignInFormCSS.registerPrompt}>Don't have an account? <a href="/register">Register Now</a></p>
+                <p className={SignInFormCSS.registerPrompt}>Don't have an account?  <Link to='/SignUp'>Register Now</Link></p>
             </div>
-        </div>
+            <ToastContainer/>
+        </form>
     );
 };
 
